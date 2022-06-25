8e3a29ad0ef41a110e328e97c17cebfdbb8cbae5
Aeroplain350 <70425458+Aeroplain350@users.noreply.github.com>
2022-06-25 14:03:40 +1000

    added audio assets
    need to add asteroid destroy sfx

---
+++ SpaceMiner/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+// manages the sound clips
+// thanks brackeys
+public class AudioManager : MonoBehaviour
+{
+    [Header("Scripts")]
+    public MixerManager mixerManager;
+
+    [Header("Sound Clips")]
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        // looks through the sound clips for the same name
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        // returns if the sound is not found
+        if (s == null) return;
+
+        // gets the sound clips enum sound type
+        string soundType = s.soundType.ToString();
+
+        // finds the mixer from the resources folder in assets
+        AudioMixer mixer = Resources.Load("AudioMixer") as AudioMixer;
+
+        // finds the specific mixer and sets
+        AudioMixerGroup[] audioMixerGroup = mixer.FindMatchingGroups(soundType);
+        s.source.outputAudioMixerGroup = audioMixerGroup[0];
+
+        s.source.pitch = s.pitch;
+        s.source.Play();
+    }
+}
---
+++ SpaceMiner/Assets/Scripts/Audio/MixerManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+// mixer mechanics
+public class MixerManager : MonoBehaviour
+{
+    [Header("Mixers")]
+    public AudioMixer audioMixer;
+
+    [Header("Game Objects")]
+    public GameObject settingsGameObject;
+
+    public Slider masterSlider, musicSlider, sfxSlider;
+
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string MasterPref = "MasterPref";
+    private static readonly string MusicPref = "MusicPref";
+    private static readonly string SFXPref = "SFXPref";
+
+    private int firstPlayInt;
+
+    [HideInInspector]
+    public float masterFloat, musicFloat, sfxFloat;
+
+    public void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+        if (firstPlayInt == 0)
+        {
+            masterFloat = 1f;
+            musicFloat = 1f;
+            sfxFloat = 1f;
+            masterSlider.value = masterFloat;
+            musicSlider.value = musicFloat;
+            sfxSlider.value = sfxFloat;
+
+            PlayerPrefs.SetFloat(MasterPref, masterFloat);
+            PlayerPrefs.SetFloat(MusicPref, musicFloat);
+            PlayerPrefs.SetFloat(SFXPref, sfxFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+            masterFloat = PlayerPrefs.GetFloat(MasterPref);
+            masterSlider.value = masterFloat;
+            musicFloat = PlayerPrefs.GetFloat(MusicPref);
+            musicSlider.value = musicFloat;
+            sfxFloat = PlayerPrefs.GetFloat(SFXPref);
+            sfxSlider.value = sfxFloat;
+        }
+    }
+
+    public void Update()
+    {
+        if (settingsGameObject.activeInHierarchy)
+        {
+            masterFloat = masterSlider.value;
+            musicFloat = musicSlider.value;
+            sfxFloat = sfxSlider.value;
+        }
+    }
+
+    public void SaveSoundSetting()
+    {
+        PlayerPrefs.SetFloat(MasterPref, masterSlider.value);
+        PlayerPrefs.SetFloat(MusicPref, musicSlider.value);
+        PlayerPrefs.SetFloat(SFXPref, sfxSlider.value);
+    }
+
+    //public void OnApplicationFocus(bool inFocus)
+    //{
+    //    if (!inFocus)
+    //    {
+    //        SaveSoundSetting();
+    //    }
+    //}
+
+    public void UpdateSound()
+    {
+        audioMixer.SetFloat("MasterVol", Mathf.Log10(masterFloat <= 0 ? 0.001f : masterFloat) * 30f);
+        audioMixer.SetFloat("MusicVol", Mathf.Log10(musicFloat <= 0 ? 0.001f : musicFloat) * 30f);
+        audioMixer.SetFloat("SFXVol", Mathf.Log10(sfxFloat <= 0 ? 0.001f : sfxFloat) * 30f);
+    }
+}
